d1a3abd357ce13980ce6b97900d61cf486b2567d
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-10 16:49:06 +0200

    Exercise 4.7.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -3,22 +3,28 @@ const { blogs, listWithOneBlog } = require('./data_for_testing');
 
 // TOTAL LIKES ===========================
 describe('total likes', () => {
-  test('when list has only one blog equals the likes of that', () => {
+  test('when list has only one blog return an object with the author and his/her likes', () => {
     const result = totalLikes(listWithOneBlog);
 
-    expect(result).toBe(5);
+    expect(result).toMatchObject({ author: 'Edsger W. Dijkstra', likes: 5 });
   });
 
-  test('when list has many blogs add up all the likes', () => {
-    const result = totalLikes(blogs);
+  // test('when list has many blogs add up all the likes', () => {
+  //   const result = totalLikes(blogs);
 
-    expect(result).toBe(36);
-  });
+  //   expect(result).toBe(36);
+  // });
 
   test('when list is empty the return value should be 0', () => {
     const result = totalLikes([]);
 
     expect(result).toBe(0);
+  });
+
+  test('when list has many blogs return the author with the most amount of likes as an onject. The return value also contains the number of likes the top author has', () => {
+    const result = totalLikes(blogs);
+
+    expect(result).toMatchObject({ author: 'Edsger W. Dijkstra', likes: 17 });
   });
 });
 
--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,7 +1,30 @@
 exports.totalLikes = blogs => {
-  const reducer = (likesSum, blog) => likesSum + blog.likes;
+  // const reducer = (likesSum, blog) => likesSum + blog.likes;
+  // return blogs.reduce(reducer, 0);
+  if (blogs.length === 0) return 0;
 
-  return blogs.reduce(reducer, 0);
+  let sumsObj = {};
+  let returnObj = {
+    author: '',
+    likes: 0,
+  };
+
+  blogs.forEach(blog => {
+    if (sumsObj[blog.author]) {
+      sumsObj[blog.author] += blog.likes;
+    } else {
+      sumsObj[blog.author] = blog.likes;
+    }
+  });
+
+  for (let key in sumsObj) {
+    if (sumsObj[key] >= returnObj.likes) {
+      returnObj.author = key;
+      returnObj.likes = sumsObj[key];
+    }
+  }
+
+  return returnObj;
 };
 
 exports.mostBlogs = blogs => {
