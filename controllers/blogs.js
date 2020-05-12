4b9109132355625b38cdb7e9b9366f5ee8de2153
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-12 12:09:45 +0200

    Bsaic CRUD Functionalities implemented

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -11,13 +11,15 @@ blogsRouter.get('/', async (req, res) => {
   res.send(blogs);
 });
 
-// @desc    Get a single post
+// @desc    Get a single blog
 // @route   GET /api/blogs/:id
 // @access  Public
 blogsRouter.get('/:id', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blog = await Blog.findById(req.params.id);
 
-  res.send(blogs);
+  if (!blog) return res.status(404).json({ error: 'Blog not found' });
+
+  res.send(blog);
 });
 
 // @desc    Post a new blog
@@ -28,6 +30,32 @@ blogsRouter.post('/', async (req, res) => {
   await blog.save();
 
   res.status(201).json(blog);
+});
+
+// @desc    Update a blog
+// @route   PUT /api/blogs/:id
+// @access  Public
+blogsRouter.put('/:id', async (req, res) => {
+  let blog = await Blog.findById(req.params.id);
+
+  if (!blog) return res.status(404).json({ error: 'Blog not found' });
+
+  blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  });
+
+  res.send(blog);
+});
+
+// @desc    Delete a blog
+// @route   DELETE /api/blogs/:id
+// @access  Public
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ message: 'Blog has been removed' });
 });
 
 module.exports = blogsRouter;
